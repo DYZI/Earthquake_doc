a41c4103a00816b87c91ede7f5f14621853c4c56
DYZI <zidnazaire@yahoo.fr>
2019-04-30 12:11:35 +0000

    test_eq_map.R

--- tests/testthat/test_eq_map.R
+++ tests/testthat/test_eq_map.R
@@ -2,7 +2,6 @@ context('test map is created')
 
 test_that('map is created ', {
   qs <- eq_load_clean_data()
-
   qs <- qs %>%
     dplyr::filter(COUNTRY == 'JAPAN') %>%
     dplyr::filter(lubridate::year(DATE) >= 2000)
